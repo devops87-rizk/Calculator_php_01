408679ab28168d74d8b121d7e496afd06e43cf93
Rizky Rahman <rizkpython87@gmail.com>
2024-08-12 21:54:35 +0800

    form php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Simple Calculator</title>
+</head>
+<body>
+
+    <form action="" method="post">
+        Number 1 <input type="text" name="number1"> <br>
+        Number 2 <input type="text" name="number1"> <br>
+        Operator <select name="operator" id=""> 
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="x">x</option>
+            <option value="/">/</option>
+        </select>
+        <button type="submit" name="eksekusi">Eksekusi</button>
+    </form>
+    
+</body>
+</html>
